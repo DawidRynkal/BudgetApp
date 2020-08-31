fd7955090e52d42e82242d10d8bc6c75ad22d985
Dawid Rynkal <dawidos14051997@gmail.com>
2020-08-31 17:26:24 +0200

    Updates

---
+++ precache-manifest.53a57f99257ba3ad6c5cc878a5a99b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d8348e39e2bed1b68ee328e5f980e4",
+    "url": "/BudgetApp/index.html"
+  },
+  {
+    "revision": "0c1265ab5b66e56480fd",
+    "url": "/BudgetApp/static/js/2.894d3967.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/BudgetApp/static/js/2.894d3967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a2da6e97ed42ff80ec",
+    "url": "/BudgetApp/static/js/main.25450cb7.chunk.js"
+  },
+  {
+    "revision": "dae39dcf098e4bdcd82f",
+    "url": "/BudgetApp/static/js/runtime-main.a036a754.js"
+  }
+]);
